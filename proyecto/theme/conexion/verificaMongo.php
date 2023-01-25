45335adb34318cb7f733e93df71006cbaa5110d4
lokiiiv <espinozaalan4567@gmail.com>
2023-01-25 10:06:06 -0600

    Corrección del bug que repite datos en la colleción de instrumentaciones cada que se registrar un nuevo docente o usuario

--- proyecto/theme/conexion/verificaMongo.php
+++ proyecto/theme/conexion/verificaMongo.php
@@ -2,13 +2,15 @@
     require_once 'conexionNoSQL.php';
     $connNoSQL = connNoSQL::singleton();
     $projeccion = ["projection" => ["correo"=>1,"_id"=>0]];
-    $docente = $connNoSQL->consultaProjeccion("docentes",["correo"=>$_SESSION["correo"]],$projeccion);
+    $docente = $connNoSQL->consultaProjeccion("docentes", ["correo" => $_SESSION["correo"]], $projeccion);
     if(!isset($docente[0]->correo)) {
         //Creación de la colección "docentes"
-        $connNoSQL->insertar("docentes",["correo"=>$_SESSION["correo"],"nombre"=>$_SESSION["nombreCompleto"]]);
-        //Creación de la colección "instrumentaciones"
-        //echo "si";
-        $connNoSQL->insertar("instrumentaciones",["Instrumentos"=>"Carreras"]);
+        $connNoSQL->insertar("docentes", ["correo" => $_SESSION["correo"], "nombre" => $_SESSION["nombreCompleto"]]);
+        
+        //Creación de la colección "instrumentaciones" solo si no existe
+        $instrumentaciones = $connNoSQL->consultaFiltrada("instrumentaciones", ["Instrumentos" => "Carreras"]);
+        if(count($instrumentaciones) == 0 || is_null($instrumentaciones)) {
+            $connNoSQL->insertar("instrumentaciones", ["Instrumentos" => "Carreras"]);
+        }
     }
-    //$connNoSQL->eliminar("prueba",["correo"=>"papasbird@itesa.edu.mx"]);
 ?>
