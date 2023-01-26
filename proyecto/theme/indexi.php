7b565b260fb8ac977011c0cf81fd2d7c1008765d
lokiiiv <espinozaalan4567@gmail.com>
2023-01-26 16:56:29 -0600

    Cambio menor

--- proyecto/theme/indexi.php
+++ proyecto/theme/indexi.php
@@ -425,7 +425,7 @@ require_once("../../valida.php");
                     var ins = document.getElementById("tablaInstrumentaciones");
                     ins.rows[x].cells[1].getElementsByTagName("input")[0].value = mater;
 
-                    //Mostrar la cantidad de temas de la materia, si no hay valor es nulo entonces mostrar el número 1 en el input
+                    //Mostrar la cantidad de temas de la materia, si no hay valor es nulo entonces mostrar el número 1 en el input de cantidad de temas
                     ins.rows[x].cells[2].getElementsByTagName("input")[0].value = respuesta.split("-")[1] != "" && respuesta.split("-")[1] != null ? parseInt(respuesta.split("-")[1]) : 1;
                 }
             }).fail(function(jqXHR, textStatus, errorThrown ) {
