bd9ffb7b146a74e0957aef19d90313dbb16871fc
lokiiiv <espinozaalan4567@gmail.com>
2023-05-31 17:04:59 -0600

    Se comenzo a verificar si el usuario que ingresa es alumno, verificando si su correo corresponde a una matricula.

--- loginGoogle.php
+++ loginGoogle.php
@@ -1,4 +1,3 @@
-
 <?php
 require_once 'config.php';
 require_once 'proyecto/theme/manejo-usuarios/UsuarioPrivilegiado.php';
@@ -26,7 +25,6 @@ if (isset($token["error"]) != "invalid_grant") {
 	$gpUserData['email'] = !empty($gpUserProfile['email']) ? $gpUserProfile['email'] : '';
 	$gpUserData['picture'] = !empty($gpUserProfile['picture']) ? $gpUserProfile['picture'] : '';
 
-
 	//Verificar que la cuenta que este ingresando sea del dominio de itesa.edu.mx
 	if (explode('@', $gpUserData['email'])[1] == 'itesa.edu.mx') {
 		//Verificar si el docente o usuario ya existe en la base de datos
@@ -34,7 +32,38 @@ if (isset($token["error"]) != "invalid_grant") {
 		require_once 'proyecto/theme/conexion/conexionSQL.php';
 		$connSQL = connSQL::singleton();
 		$query = "Select cat_ID, cat_Nombre, cat_ApePat, cat_ApeMat from docentes where cat_CorreoE='" . $gpUserData['email'] . "'";
-		$docente = $connSQL->consulta($query);
+		$usuario = $connSQL->consulta($query);
+
+		//El usuario existe, independientemente de su rol, permitir el acceso y guardar las variables de sesion
+		if(count($usuario) > 0 && !is_null($usuario)) {
+			//Insert or update user data to the database 
+			// Storing user data in the session 
+			$_SESSION["correo"] = $gpUserData["email"];
+			$_SESSION["userData"] = $gpUserData;
+
+			$_SESSION["nombreCompleto"] = $usuario[0][1] . " " . $usuario[0][2] . " " . $usuario[0][3];
+			$_SESSION["idUsuario"] = $usuario[0][0];
+			
+			require_once './proyecto/theme/conexion/verificaMongo.php';
+
+			//Redirigir a la pagina principal
+			header("Location:proyecto/theme/index.php");
+		} else {
+			//Si no existe, no permitir el acceso...
+			//Pero hay que verificar si el usuario que intenta acceder es un alumno, si es asi, registrar automaticamente como usuario nuevo y alumno.
+			$posibleMatricula = explode('@', $gpUserData['email'])[0];
+			//Verificar que sea una matricula de 8 digitos
+			$regexMatricula = "/^[0-9]{8}$/";
+			if(preg_match($regexMatricula, $posibleMatricula)) {
+				echo '<p>Eres alumno.</p>';
+			} else {
+				//Si no coincide, son correos que no pertenecen a alumnos
+				header("Location: index.php?mensaje=2");
+				die();	
+			}
+		}
+
+		/* $docente = $connSQL->consulta($query);
 
 		if (count($docente) > 0 && !is_null($docente)) {
 			//Insert or update user data to the database 
@@ -53,7 +82,7 @@ if (isset($token["error"]) != "invalid_grant") {
 			//Si el usuario no existe aun en el catalgo, redirigir con su mensaje
 			header("Location: index.php?mensaje=2");
 			die();	
-		}
+		} */
 
 	} else {
 		//Redireccionar al index, mandando un mensaje de alerta
